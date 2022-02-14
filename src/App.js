66eea1b4be63d7cecb64b961de378f68462a6b4a
Motoki <report.japan.hiroshima@gmail.com>
2022-02-14 12:54:10 +0900

    Form for first and last names with state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const [formData, setFormData] = React.useState({
+    firstName: "",
+    lastName: ""
+  })
+
+  console.log(formData)
+
+  function handleChange(event) {
+    setFormData(prevFormData => {
+      return {
+        ...prevFormData,
+        // [event.target.name] is a computed property
+        [event.target.name]: event.target.value
+      }
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input
+          type='text'
+          placeholder='First name'
+          onChange={handleChange}
+          name='firstName'
+        />
+        <input
+          type='text'
+          placeholder='Last name'
+          onChange={handleChange}
+          name='lastName'
+        />
+      </form>
     </div>
   );
 }

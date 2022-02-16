16b295e56aeb030bc45f527a9a61a3ce1955fc18
Motoki <report.japan.hiroshima@gmail.com>
2022-02-16 12:29:46 +0900

    Updated a form with radio button

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import React from 'react';
 
 function App() {
-  const [formData, setFormData] = React.useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    comments: "",
-    isFriendly: true
-  })
+  const [formData, setFormData] = React.useState(
+    {
+      firstName: "",
+      lastName: "",
+      email: "",
+      comments: "",
+      isFriendly: true,
+      employment: ""
+    }
+  )
 
   console.log(formData)
 
@@ -60,6 +63,43 @@ function App() {
           name='isFriendly'
         />
         <label htmlFor='isFriendly'>Are you friendly?</label>
+
+        <fieldset>
+          <legend>Current employment status</legend>
+
+          <input
+            type='radio'
+            id='unemployed'
+            name='employment'
+            value='unemployed'
+            checked={formData.employment === "unemployed"}
+            onChange={handleChange}
+          />
+          <label htmlFor='unemployed'>Unemployed</label>
+          <br />
+
+          <input
+            type='radio'
+            id='part-time'
+            name='employment'
+            value='part-time'
+            checked={formData.employment === "part-time"}
+            onChange={handleChange}
+          />
+          <label htmlFor='part-time'>Part-time</label>
+          <br />
+
+          <input
+            type='radio'
+            id='full-time'
+            name='employment'
+            value='full-time'
+            checked={formData.employment === "full-time"}
+            onChange={handleChange}
+          />
+          <label htmlFor='full-time'>Full-time</label>
+          <br />
+        </fieldset>
       </form>
     </div>
   );
